95e4a3f6a5e5f35a31bc5bfc76ed8df363994b89
takakha <66158185+takakha@users.noreply.github.com>
2020-08-08 11:34:34 +0900

    ユーザー情報に関するバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :nickname
+          validates :birthday
+          validates :email
+                              
+          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX
+       
+          with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+            validates :first_name
+            validates :last_name
+          end
+       
+          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+            validates :first_name_kana
+            validates :last_name_kana
+          end
+        end
+ 
+
 end
