6c2fb832550905d2a9300ec5760793c03cd4951a
takakha <66158185+takakha@users.noreply.github.com>
2020-08-10 18:47:39 +0900

    商品をDBに保存、Top画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  
+  def create
+    @item = Item.new(item_params)
+     if @item.save
+      redirect_to root_path
+     else
+      render :new
+     end
+  end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item, :image, :text, :price,:category_id ,:status_id ,:delivery_fee_id , :area_id, :day_id).merge(user_id: current_user.id)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day
 
    #空の投稿を保存できないようにする
-   validates :item, :text, :price,:category_id ,:status_id ,:delively_fee_id , :area_id, :day_id,  presence: true
+   validates :item, :image, :text, :price,:category_id ,:status_id ,:delivery_fee_id , :area_id, :day_id,  presence: true
 
    #ジャンルの選択が「--」の時は保存できないようにする
-   validates :category_id, :status_id, :delively_fee_id, :area_id, :day_id, numericality: { other_than: 1 }
+   validates :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, numericality: { other_than: 1 }
 
-has_one_attached :image
-belongs_to :user
-has_one :order
-has_many :addresses, through: items_addresses
+  has_one_attached :image
+  belongs_to :user
+  has_one :order
+  
 
 end
