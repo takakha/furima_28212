a39e74cf88a7d7009793e816fb5cf55b8ac7ffb3
takakha <66158185+takakha@users.noreply.github.com>
2020-08-14 16:23:25 +0900

    アソシエーション、formsフォルダ作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,15 @@ class OrdersController < ApplicationController
 
   def index
   end
+  
 
+  def create
+    @oder = Oder.create(order_params)
+  end
+
+  private
+
+  def Oder_params
+    params.require(:item).permit()
+  end
 end
---
+++ app/forms/item_oder.rb
@@ -0,0 +1,6 @@
+class ItemOder
+
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number
+
+end
