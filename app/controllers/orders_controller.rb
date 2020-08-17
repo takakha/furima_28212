4069c63ba5393bb509e9bd8e418f11ec2a5eb8a1
takakha <66158185+takakha@users.noreply.github.com>
2020-08-17 15:23:41 +0900

    購入内容の情報をビューに反映

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
   end
   
 
@@ -19,6 +20,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number,:item_id).merge(user_id: current_user.id)
+    params.permit(:token, :postcode, :prefecture_id, :city, :block, :building, :phone_number,:item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類
+    )
   end
 end
