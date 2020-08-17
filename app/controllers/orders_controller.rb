d220d92cbecc39c97707cb11ae6686e5febd779b
takakha <66158185+takakha@users.noreply.github.com>
2020-08-17 16:04:57 +0900

    tokenをDBに保存しない記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,9 @@ class OrdersController < ApplicationController
 
   def create
     
-    @order = OrderAddress.new(order_params)
+    @order = OrderAddress.new(postcode: order_params[:postcode], prefecture_id: order_params[:prefecture_id],
+    city: order_params[:city], block: order_params[:block], building: order_params[:building],phone_number: order_params[:phone_number],
+     item_id: order_params[:item_id],user_id: order_params[:user_id])
     
     if @order.valid?
       @order.save  # バリデーションをクリアした時
@@ -23,10 +25,14 @@ class OrdersController < ApplicationController
     params.permit(:token, :postcode, :prefecture_id, :city, :block, :building, :phone_number,:item_id).merge(user_id: current_user.id)
   end
 
+  def info_item
+      @item = Item.find(params[:id])
+  end       
+
   def pay_item
-    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = "sk_test_6614d54c67e208a9f3a5c6d7"# PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類
     )
