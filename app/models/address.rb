caf042418b5471957eb5b5e4977a34f38e277044
takakha <66158185+takakha@users.noreply.github.com>
2020-08-19 18:27:07 +0900

    正規表現を変数に代入してバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,10 +4,12 @@ class Address < ApplicationRecord
   belongs_to :item
   
   with_options presence: true do
-    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}    
+    VAVID_POST_REGEX =  /\A[0-9]{3}-[0-9]{4}\z/.freeze
+    validates_format_of :postcode,  with: VAVID_POST_REGEX  
     validates :city
     validates :block
-    validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/}
+    VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/.freeze
+    validates_format_of :phone_number,  with: VALID_PHONE_REGEX 
     validates :prefecture_id
   end
   validates :prefecture_id, numericality: { other_than: 1 }
