f42e2d2e175187c671ef5a8427d7a7d3e00bcd78
takakha <66158185+takakha@users.noreply.github.com>
2020-08-19 18:09:13 +0900

    calc.jsの変数宣言の記述を修正

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,11 +1,11 @@
 function calc(){
 
-  var addTaxPrice = document.getElementById("add-tax-price")
-  var profit = document.getElementById("profit")
-  var itemPrice =  document.getElementById("item-price")
+  const addTaxPrice = document.getElementById("add-tax-price")
+  const profit = document.getElementById("profit")
+  const itemPrice =  document.getElementById("item-price")
 
    itemPrice.addEventListener('input', function(){
-    var inputs = itemPrice.value
+    const inputs = itemPrice.value
     addTaxPrice.innerHTML = `${Math.floor(inputs * 0.1)}`
     profit.innerHTML = `${Math.floor(inputs * 0.9)}`
    })
